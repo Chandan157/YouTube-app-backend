0e1547ccae799b1b1bbb5ea1781bdd9712a70230
Chandan <94438598+Chandan157@users.noreply.github.com>
2024-03-27 20:53:43 +0530

    add registr controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "./../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);    // agar /register hit hoga tab registerUser method call ho jayega
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount:1,
+    },
+  ]),
+  registerUser
+); // agar /register hit hoga tab registerUser method call ho jayega
 export default router;
