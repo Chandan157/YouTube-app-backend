fdf7d7c1a216806d8f0bf03f769e58af086cb1b9
Chandan <94438598+Chandan157@users.noreply.github.com>
2024-04-04 10:53:40 +0530

    comments routes added

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -16,7 +18,9 @@ app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 //routes decleration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/comments", commentRouter);
 export { app };
