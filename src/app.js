bd9465c700e018975c67fd043b58ee11c9156f1d
Chandan <94438598+Chandan157@users.noreply.github.com>
2024-04-07 19:54:22 +0530

    subscription controller added

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import dashboardRouter from "./routes/dashboard.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js"
 
 //routes decleration
 app.use("/api/v1/users", userRouter);
@@ -31,5 +32,6 @@ app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter)
 
 export { app };
